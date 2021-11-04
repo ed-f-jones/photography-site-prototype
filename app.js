a971c07c63a763daf3874a3aa9159066101fcc35
Edward Jones <ed.f.jones@gmail.com>
2021-11-03 22:38:18 -0400

    gallery complete with nav functions

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const navBtn = document.getElementById("nav-toggle");
 const links = document.getElementById("nav-links");
 const navIcon = document.querySelectorAll(".nav-icon");
+const scrollLinks = document.querySelectorAll(".scroll-link");
 
 navBtn.addEventListener("click", ()=> {
     links.classList.toggle("show-links");
 });
 
 
-links.addEventListener("click", ()=> {
-    links.classList.remove("show-links")
+scrollLinks.forEach(link => {
+    links.addEventListener("click", e => {
+        e.preventDefault();
+        links.classList.remove("show-links");
+
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        let position = element.offsetTop - 62;
+        window.scrollTo({
+            left: 0,
+            //top: element.offset,
+            top: position,
+            behavior: "smooth"
+        });
+    });
 });
